f0a2794144d3dc72d0782f953e0a86008633db4d
Rizki Alfian <rizkialfiansss@yahoo.com>
2019-07-06 23:46:31 +0700

    Tutorial RESTful API dengan Laravel '09 Membuat Post' by kawankoding (done)

--- app/Transformers/UserTransformer.php
+++ app/Transformers/UserTransformer.php
@@ -10,6 +10,7 @@ class UserTransformer extends TransformerAbstract
     public function transform(User $user)
     {
         return [
+            'id'    => $user->id,
             'name'  => $user->name, //yg disebelah kiri yg di tampilkan di return, sebelah kanan dari tablenya
             'email' => $user->email,
             'registered' => $user->created_at->diffForHumans(),
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,4 @@ Route::post('auth/register', 'AuthController@register');
 Route::post('auth/login', 'AuthController@login');
 Route::get('users', 'UserController@users');
 Route::get('users/profile', 'UserController@profile')->middleware('auth:api');
+Route::post('post', 'PostController@add')->middleware('auth:api');
